fa42fc139b4cbbad58d0079685a6ab864f454d6f
Raisa <raisa.abzelimova@yandex.ru>
2017-10-14 18:14:58 +0300

    Tests

--- homework3/game.py
+++ homework3/game.py
@@ -23,7 +23,20 @@ def shuffle_field():
     :return: list with 16 randomly shuffled tiles,
     one of which is a empty space.
     """
-    pass
+
+    field = [i for i in range(1, 16)]
+    field.append(EMPTY_MARK)
+
+    for _ in range(100):
+        move = random.choice(list(MOVES.keys()))
+        try:
+            field = perform_move(field, move)
+        except IndexError:
+            pass
+
+    # random.shuffle(field)
+
+    return field
 
 
 def print_field(field):
@@ -32,7 +45,14 @@ def print_field(field):
     :param field: current field state to be printed.
     :return: None
     """
-    pass
+
+    print('-' * 12)
+    i = 0
+    while i < 4:
+        print('|'.join([str(title).ljust(2) for
+                        title in field[i * 4:i * 4 + 4]]))
+        i += 1
+        print('-' * 12)
 
 
 def is_game_finished(field):
@@ -41,7 +61,11 @@ def is_game_finished(field):
     :param field: current field state.
     :return: True if the game is finished, False otherwise.
     """
-    pass
+    for i in range(1, len(field)):
+        if field[i - 1] != i or field[i - 1] == EMPTY_MARK:
+            return False
+
+    return True
 
 
 def perform_move(field, key):
@@ -52,7 +76,23 @@ def perform_move(field, key):
     :return: new field state (after the move).
     :raises: IndexError if the move can't me done.
     """
-    pass
+
+    index_x = field.index(EMPTY_MARK)
+
+    if index_x % 4 == 0 and key == 'a':
+        raise IndexError()
+    elif index_x % 4 == 3 and key == 'd':
+        raise IndexError()
+    elif index_x < 4 and key == 'w':
+        raise IndexError()
+    elif index_x > 11 and key == 's':
+        raise IndexError()
+
+    new_field = list(field)
+    new_field[index_x], new_field[index_x + MOVES[key]] =\
+        new_field[index_x + MOVES[key]], new_field[index_x]
+
+    return new_field
 
 
 def handle_user_input():
@@ -64,7 +104,11 @@ def handle_user_input():
         'd' - right
     :return: <str> current move.
     """
-    pass
+    move = None
+    while move not in MOVES.keys():
+        move = input('Use "wasd" for step:\n')
+
+    return move
 
 
 def main():
@@ -73,11 +117,29 @@ def main():
     It also calls other methods.
     :return: None
     """
-    pass
+
+    field = shuffle_field()
+    amount_moves = 0
+
+    while True:
+        try:
+            print_field(field)
+            key = handle_user_input()
+            field = perform_move(field, key)
+            amount_moves += 1
+            if is_game_finished(field):
+                print('You`re win! You have made {} steps').format(amount_moves)
+                break
+
+        except IndexError:
+            print('It`s not possible. Try again')
 
 
 if __name__ == '__main__':
     # See what this means:
     # http://stackoverflow.com/questions/419163/what-does-if-name-main-do
 
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('shutting down')
