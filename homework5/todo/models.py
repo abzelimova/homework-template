148d063a134f271a18b052b8c900ac1a11f1c273
Raisa <raisa.abzelimova@yandex.ru>
2017-10-14 17:29:24 +0300

    First commit

--- homework5/todo/models.py
+++ homework5/todo/models.py
@@ -40,7 +40,12 @@ class BaseItem(object):
 
 class ToDoItem(BaseItem):
     def __str__(self):
-        return 'ToDo: {}'.format(self.heading)
+        if self.done:
+            done = '+'
+        else:
+            done = '-'
+        return '{} ToDo: {}'.format(done, self.heading)
+
 
     @classmethod
     def construct(cls):
@@ -54,7 +59,12 @@ class ToBuyItem(BaseItem):
         self.price = price
 
     def __str__(self):
-        return 'ToBuy: {} for {}'.format(
+        if self.done:
+            done = '+'
+        else:
+            done = '-'
+        return '{} ToBuy: {} for {}'.format(
+            done,
             self.heading,
             self.price,
         )
@@ -64,3 +74,25 @@ class ToBuyItem(BaseItem):
         heading = input('Input heading: ')
         price = input('Input price: ')
         return cls(heading, price)
+
+class ToReadItem(BaseItem):
+    def __init__(self, heading, url):
+        super().__init__(heading)
+        self.url = url
+
+    def __str__(self):
+        if self.done:
+            done = '+'
+        else:
+            done = '-'
+        return '{} ToRead: {} {}'.format(
+            done,
+            self.heading,
+            self.url,
+        )
+
+    @classmethod
+    def construct(cls):
+        heading = input('Input heading: ')
+        url = input('Input url: ')
+        return cls(heading, url)
