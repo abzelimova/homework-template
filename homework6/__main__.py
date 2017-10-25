d8c577dfff0ebf20c4c771da4486ae8ca9fd6578
Raisa <raisa.abzelimova@yandex.ru>
2017-10-25 21:04:18 +0300

    check the misstakes

--- homework6/__main__.py
+++ homework6/__main__.py
@@ -2,6 +2,8 @@ import json
 import random
 
 consignment = 0
+
+
 def is_english_letters(word):
     alphabet = []
     for i in range(97, 123):
@@ -15,29 +17,37 @@ def is_english_letters(word):
             return False
 
 
-def KillAPerson(param): #Вывод трупика
+def kill_a_person(param):  # Вывод трупика
     if param == 0:
-        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ', ' ', ' ', '|', ' ', ' ', '', '|', ' ', ' ', '']
+        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ',
+                 ' ', ' ', '|', ' ', ' ', '', '|', ' ', ' ', '']
     if param == 1:
-        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ', '0', ' ', '|', ' ', ' ', '', '|', ' ', ' ', '']
+        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ',
+                 '0', ' ', '|', ' ', ' ', '', '|', ' ', ' ', '']
     if param == 2:
-        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ', '0', ' ', '|', ' ', '|', '', '|', ' ', ' ', '']
+        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ',
+                 '0', ' ', '|', ' ', '|', '', '|', ' ', ' ', '']
     if param == 3:
-        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ', '0', ' ', '|', '-', '|', ' ','|', ' ', ' ', '']
+        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ',
+                 '0', ' ', '|', '-', '|', ' ','|', ' ', ' ', '']
     if param == 4:
-        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ', '0', ' ', '|', '-', '|', '-', '|', ' ', ' ', '']
+        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ',
+                 '0', ' ', '|', '-', '|', '-', '|', ' ', ' ', '']
     if param == 5:
-        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ', '0', ' ', '|', '-', '|', '-', '|', ' ', '| ', '']
+        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ',
+                 '0', ' ', '|', '-', '|', '-', '|', ' ', '| ', '']
     if param >= 6:
-        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ', '0', ' ', '|', '-', '|', '-', '|', ' | | ', '']
+        field = ['|', '-', '-', ' ', '|', ' ', '|', ' ', '|', ' ',
+                 '0', ' ', '|', '-', '|', '-', '|', ' | | ', '']
     print_line(field)
 
-def print_line(field): #рисует правильного трупика
+
+def print_line(field):  # рисует правильного трупика
     n = 4
     print('------------------------')
     for i in range(0, len(field), n):
         prepared_line = ['{:2s}'.format((x)) for x in field[i:i + n]]
-        print (' %s' % ''.join(prepared_line))
+        print(' %s' % ''.join(prepared_line))
     print('------------------------')
 
 
@@ -45,7 +55,7 @@ class LogicOfGame(object):
     def __init__(self, word):
         self.word = word
         self.change_of_letters = str('_' * len(word))
-        self.letter = ""
+        self.letter = ''
         self.param = 0
         self.guessed_letters = 0
         self.sum_of_letters = 0
@@ -54,20 +64,21 @@ class LogicOfGame(object):
         if self.letter not in self.word:
             print('Такой буквы нет')
             self.param += 1
-            KillAPerson(self.param)
+            kill_a_person(self.param)
             return False
         else:
             i = 0
             while i < len(self.word):
                 if self.word[i] == self.letter:
                     self.guessed_letters += 1
-                    self.change_of_letters = self.change_of_letters[:i] + self.letter + self.change_of_letters[i + 1:]
+                    self.change_of_letters = self.change_of_letters[:i] + \
+                                             self.letter + self.change_of_letters[i + 1:]
                 i += 1
             return True
 
     def is_finish_game(self):
         if self.param >= 6 or self.change_of_letters == self.word:
-            print("Игра окончена")
+            print('Игра окончена')
             return True
         return False
 
@@ -77,6 +88,7 @@ class LogicOfGame(object):
             new += i + ' '
         return new
 
+
 def start_game(game_word):
     game = LogicOfGame(game_word)
     while not game.is_finish_game():
@@ -85,18 +97,21 @@ def start_game(game_word):
         game.sum_of_letters += 1
         game.is_right_letter()
     if game.param >= 6:
-        print("Вы проиграли.Количество попыток: " , game.sum_of_letters, 'Количество угаданых букв', game.guessed_letters)
+        print('Вы проиграли.Количество попыток: ', game.sum_of_letters,
+              'Количество угаданых букв', game.guessed_letters)
         return False
     else:
-        print("Вы выиграли.Количество попыток: ", game.sum_of_letters, 'Количество угаданых букв', game.guessed_letters)
+        print('Вы выиграли.Количество попыток: ', game.sum_of_letters,
+              'Количество угаданых букв', game.guessed_letters)
         return True
+
 
 def main():
     first_victory = 0
     second_victory = 0
-    round = 1
+    round_count = 1
 
-    choice = input('Введите 1 если хотите играть с человеком, 2 - с компьютером. Мой выбор: ')
+    choice = input('Введите 1 если хотите играть с человеком,\n\r 2 - с компьютером. Мой выбор: ')
     print(choice)
     if choice == '1':
         while first_victory < 2 and second_victory < 2:
@@ -105,12 +120,12 @@ def main():
                 print('Используйте только английские слова')
                 game_word = input('Введите слово:')
             if start_game(game_word):
-                if round % 2 == 1:
+                if  round_count % 2 == 1:
                     first_victory += 1
                 else:
                     second_victory += 1
-            round += 1
-            print("Счет: ", first_victory, " : ", second_victory)
+            round_count += 1
+            print('Счет: ', first_victory, ' : ', second_victory)
         if first_victory == 2:
             print('Победил первый игрок')
         else:
@@ -120,12 +135,10 @@ def main():
         with open('words.json', 'r', encoding='utf-8') as fh:
             game_word = random.choice(json.load(fh)['words'])
         if start_game(game_word):
-            round += 1
+            round_count += 1
 
-
-
-if __name__  == '__main__':
+if __name__ == '__main__':
         try:
             main()
         except KeyboardInterrupt:
-            print("bye")
+            print('bye')
