8067d99d0c7c81640e23dedfc26527d748a37c19
Raisa <raisa.abzelimova@yandex.ru>
2017-11-02 17:14:46 +0300

    First tests

--- homework7/functional.py
+++ homework7/functional.py
@@ -2,16 +2,32 @@ from functools import reduce
 
 
 def modulo_five():
-    pass
+    list_of_num = [1, 4, 5, 30, 99]
+    def remainder(num):
+        return num % 5
+    result = list(map(remainder, list_of_num))
+    return result
 
 
 def to_string():
-    pass
+    item = [3, 4, 90, -2]
+    def make_string(string):
+        return str(string)
+    result = list(map(make_string, item))
+    return result
 
 
 def filter_string():
-    pass
+    list_of_item = ['some', 1, 'v', 40, '3a', str]
+
+    def is_string(items):
+        return not isinstance(items, str)
+
+    no_strings = list(filter(is_string, list_of_item))
+    return no_strings
 
 
 def count_letters():
-    pass
+    list_of_word = ['some', 'other', 'value']
+    sum_all = len(reduce(lambda word, word2: word + word2, list_of_word))
+    return sum_all
--- homework7/generators.py
+++ homework7/generators.py
@@ -3,12 +3,23 @@ from datetime import datetime, timedelta
 
 
 def all_even_numbers():
-    pass
+    for i in range(0,100,2):
+        yield i
 
 
-def random_increasing_number():
-    pass
+def random_increasing_number(start_from = 0):
+    last_num = start_from
+    next_num = random.randint(last_num, last_num + 150)
+    while True:
+        next_num = random.randint(next_num, next_num+150)
+        yield next_num
 
 
 def next_day():
-    pass
+    i = 0
+    today = datetime.datetime.today().date()
+
+    while True:
+        yield today
+        today = today + datetime.timedelta(days=i)
+        i += 1
